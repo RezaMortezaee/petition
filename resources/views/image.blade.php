e1ae5ad06592415fc6c2c3221a22eaafe4f87c1a
RezaMrtz <reza.mrtz@gmail.com>
2020-11-26 17:40:26 +0330

    Image Upload

---
+++ resources/views/image.blade.php
@@ -0,0 +1,3 @@
+<x-image>
+
+</x-image>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/**
+ * Image Path
+*/
+Route::get('upload-image', [UploadImageController::class, 'index']);
+Route::post('save', [UploadImageController::class, 'save']);
